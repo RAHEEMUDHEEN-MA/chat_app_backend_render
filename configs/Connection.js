e7acfd359c7bdb315780375b761bc7ab3df977f3
RAHEEMUDHEEN-MA <raheemudheenma118@gamil.com>
2024-03-03 22:36:43 +0530

    for backend hosting-(ignored node modules)

--- configs/Connection.js
+++ configs/Connection.js
@@ -1,15 +1,18 @@
-const mongoose=require('mongoose')
+const mongoose = require('mongoose');
 
-const Connection=async()=>{
-    try {
-        const connect=await mongoose.connect(process.env.MongoURL)
-             
-      
-        console.log(`Database connected successfully`)
-    } catch (error) {
-        console.log(`failed to connect mongoDB ,error:${error}`)
-        
+const Connection = async () => {
+  try {
+    if (!process.env.MongoURL) {
+      throw new Error('Missing environment variable: MongoURL');
     }
-}
 
-module.exports=Connection
+    const connection = await mongoose.connect(process.env.MongoURL);
+    console.log(`Database connected successfully ${connection.connectionId}`);
+    return connection; // Return the connection object
+  } catch (error) {
+    console.error(`Failed to connect to MongoDB: ${error.message}`);
+    throw error; // Re-throw the error for further handling
+  }
+};
+
+module.exports = Connection;
